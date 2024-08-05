8d05b157fa8b22122e6da5e3ee51bf9040c50256
mateen3121 <shoyabmateen@gmail.com>
2024-08-05 18:48:16 +0530

    Updated portfolio files

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+const logotext = "Shaik Shoyab Mateen";
+const meta = {
+    title: "Shaik Shoyab Mateen",
+    description: "I’m Shaik Shoyab Mateen, a Frontend Developer.",
+};
+
+const introdata = {
+    title: "I’m Shaik Shoyab Mateen",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web applications",
+    },
+    description: "I am Shaik Shoyab Mateen, a Frontend Developer with 6 months of experience in creating responsive user interfaces with React and integrating RESTful APIs using Node.js and Express. My focus is on identifying and solving development challenges to ensure efficient, optimized code. At Numetry Technologies, I developed responsive UIs, enhanced application flow with React Router and Redux, and implemented a comprehensive code review process. I have also worked on notable projects such as the Super School website, an e-commerce platform for book sales, a resume builder using React, and a feedback system utilizing facial emotion recognition. With strong technical skills in HTML, CSS, JavaScript, React.js, Node.js, and databases like MongoDB and MySQL, I am eager to contribute to impactful projects and grow my technical skills further.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "I am Shaik Shoyab Mateen, a Frontend Developer with 6 months of experience in creating responsive user interfaces with React and integrating RESTful APIs using Node.js and Express. My focus is on identifying and solving development challenges to ensure efficient, optimized code. At Numetry Technologies, I developed responsive UIs, enhanced application flow with React Router and Redux, and implemented a comprehensive code review process. I have also worked on notable projects such as the Super School website, an e-commerce platform for book sales, a resume builder using React, and a feedback system utilizing facial emotion recognition. With strong technical skills in HTML, CSS, JavaScript, React.js, Node.js, and databases like MongoDB and MySQL, I am eager to contribute to impactful projects and grow my technical skills further."
+};
+
+const worktimeline = [
+    {
+        jobtitle: "Software Developer Intern",
+        where: "Numetry Technologies",
+        date: "01/2024 – Present",
+    }
+];
+
+const skills = [
+    {
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "CSS",
+        value: 75,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "React.js",
+        value: 70,
+    },
+    {
+        name: "Node.js",
+        value: 65,
+    },
+    {
+        name: "Express.js",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 60,
+    },
+    {
+        name: "MySQL",
+        value: 55,
+    }
+];
+
+const services = [
+    {
+        title: "Frontend Development",
+        description: "Creating responsive and intuitive user interfaces with React.js and related technologies.",
+    },
+    {
+        title: "Backend Development",
+        description: "Developing and integrating RESTful APIs using Node.js and Express.js.",
+    },
+    {
+        title: "Full Stack Development",
+        description: "Building full-stack applications using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://i.pinimg.com/736x/99/35/95/9935954371ca3efbe3c378cc7f8f5397.jpg",
+        description: "Super School Website",
+        link: "https://super-school.vercel.app/",
+    },
+    {
+        img: "https://static.vecteezy.com/system/resources/previews/029/329/267/large_2x/literary-treasure-trove-a-stack-of-books-beckons-at-the-bookstore-vertical-mobile-wallpaper-ai-generated-free-photo.jpg",
+        description: "E-commerce Website – Online Book Store",
+        link: "https://shoyabmateen-booksworm-den-frontend.vercel.app/",
+    },
+    {
+        img: "https://shoyabmateen-resume-bulider--pearl.vercel.app/static/media/bg.9d4f86b47acb8dbd7856.jpg",
+        description: "Resume Builder Using React",
+        link: "https://shoyabmateen-resume-bulider--pearl.vercel.app/",
+    },
+    {
+        img: "https://blog.mantratec.com/Images/post-img/lg/2018/How-Facial-Recognition-System-Is-Making-Its-Mark.jpg",
+        description: "Feedback System Using Facial Emotion Recognition",
+        link: "https://www.atlantis-press.com/proceedings/icciet-24/126002043",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shoyabmateen@gmail.com",
+    YOUR_FONE: "+91 8309698949",
+    description: "Feel free to reach out to me via email or phone for any inquiries or collaboration opportunities.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/mateen3121",
+    linkedin: "https://www.linkedin.com/in/shaik-shoyab-mateen-001b471b0/",
+    twitter: "https://x.com/MateenShoy25469",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
